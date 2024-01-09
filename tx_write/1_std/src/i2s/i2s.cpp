d8e82f8c8018cc579a214cb5ad7be85cbf5b14b1
jasu <jasu@jasubox.myguest.virtualbox.org>
2024-01-09 03:42:43 +0200

    mod buffer

--- tx_write/1_std/src/i2s/i2s.cpp
+++ tx_write/1_std/src/i2s/i2s.cpp
@@ -9,7 +9,7 @@ i2s_chan_handle_t I2S::tx_handle;
 
 const i2s_std_config_t I2S::std_conf = {
   .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(48000),
-  .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_8BIT, I2S_SLOT_MODE_MONO),
+  .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_MONO),
   .gpio_cfg = {
       .mclk = I2S_GPIO_UNUSED,
       .bclk = PIN_BCLK,
@@ -32,18 +32,24 @@ void I2S::init() {
 };
 
 void I2S::write() {
-  size_t buffer_size = 3;
-  size_t *bytes_written = 0;
-  uint8_t *buffer = (uint8_t *)heap_caps_malloc(buffer_size, MALLOC_CAP_DMA | MALLOC_CAP_8BIT);
-
-  buffer[0] = 0x01; // 0001
-  buffer[1] = 0x03; // 0011
-  buffer[2] = 0x05; // 0101
+  size_t buffer_size = 4;
+  size_t bytes_written = 0;
+  uint16_t *buffer = (uint16_t *)malloc(buffer_size);
+  
+  // Second byte for i2s buffer alignment
+  // Last 8 bits are dropped
+  buffer[0] = 0x0100;
+  buffer[1] = 0x0300;
+  buffer[2] = 0x0500;
+  buffer[3] = 0x0700;
 
   // Transferred as:
-  // ..., 0x01 0x01, 0x03 0x03, 0x05 0x05, 0x01 0x01, ...
-  // Where 1 byte transferred each WS low (I2S_DATA_BIT_WIDTH_8BIT)
+  // ..., 0x03, 0x01, 0x07, 0x05, 0x03 0x01, 0x07 0x05, ...
+  // Where 1 byte transferred each WS low
 
-  i2s_channel_write(tx_handle, buffer, buffer_size, bytes_written, portMAX_DELAY);
+  // i2s reads as bytes (uint8)
+  size_t i2s_buffer_size = buffer_size * 2;
+
+  i2s_channel_write(tx_handle, buffer, i2s_buffer_size, &bytes_written, portMAX_DELAY);
   free(buffer);
 };
