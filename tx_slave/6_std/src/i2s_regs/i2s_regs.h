99acf0806314fb039316db43dc1938060a9f42fe
jasu <jasu@jasubox.myguest.virtualbox.org>
2024-02-14 02:52:40 +0200

    add reg print

---
+++ tx_slave/6_std/src/i2s_regs/i2s_regs.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <stdint.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+class I2S_Regs {
+public:
+  // Functions
+  static void print_all();
+  static void print_I2S_FIFO_registers();
+  static void print_configuration_registers();
+  static void print_DMA_registers();
+  static void print_PDM_registers();
+  static void print_interrupt_registers();
+
+  static void print_DMA_outlink(uintptr_t outlink_addr, bool follow_links);
+
+private:
+  // Functions
+  static uint16_t get_DW0_size(uint32_t dw0);
+  static uint16_t get_DW0_length(uint32_t dw0);
+  static void print_DMA_descriptor(uint32_t* ptr, uint16_t index);
+  static void print_DMA_buffer(uint32_t* ptr, uint16_t index);
+};
