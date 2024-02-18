c63c8e18f5721af4442b05125f498b9594a90cfb
jasu <jasu@jasubox.myguest.virtualbox.org>
2024-02-18 03:39:03 +0200

    init

---
+++ rx_slave/10_std/src/main.cpp
@@ -0,0 +1,25 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "i2s/i2s.h"
+#include "driver/uart.h"
+
+uart_config_t uart_config = {
+    .baud_rate = 115200,
+    .data_bits = UART_DATA_8_BITS,
+    .parity = UART_PARITY_DISABLE,
+    .stop_bits = UART_STOP_BITS_1,
+    .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+};
+
+extern "C" {
+  void app_main(void);
+};
+
+void app_main() {
+  I2S::init();
+  while(1) {
+    vTaskDelay(pdMS_TO_TICKS(200));
+    I2S::read();
+  }
+};
